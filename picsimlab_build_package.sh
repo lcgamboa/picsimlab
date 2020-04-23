e544a2e3b84f6fa3cc5a4977d698265f4fb2893b
lcgamboa <lcgamboa@yahoo.com>
2020-04-22 23:37:44 -0300

    chg: debian control updated !minor

--- picsimlab_build_package.sh
+++ picsimlab_build_package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x 
+rm -rf build_all/picsimlab*
 sudo apt-get install debmake
 git archive --format=tar master > build_all/picsimlab-0.8.0.tar 
 cd build_all
@@ -11,7 +12,7 @@ tar  --delete --wildcards -f picsimlab-0.8.0.tar "docs/*.png"
 tar  --delete --wildcards -f picsimlab-0.8.0.tar "docs/*.html" 
 tar  --delete --wildcards -f picsimlab-0.8.0.tar "docs/*.pdf" 
 tar  --delete --wildcards -f picsimlab-0.8.0.tar "docs/*.css" 
-gzip picsimlab-0.8.0.tar
+gzip -q picsimlab-0.8.0.tar
 mkdir picsimlab-0.8.0
 cd picsimlab-0.8.0
 tar xvfz ../picsimlab-0.8.0.tar.gz
@@ -20,6 +21,7 @@ mv tools/srtank/Makefile.static tools/srtank/Makefile
 mv tools0espmsim/Makefile.static tools/espmsim/Makefile
 cd ..
 tar cvfz  picsimlab-0.8.0.tar.gz picsimlab-0.8.0
-cd picsimlab-0.8.0/
-debuild
+mv picsimlab-0.8.0.tar.gz picsimlab_0.8.0.orig.tar.gz
+cd picsimlab-0.8.0
+sudo debuild
 
