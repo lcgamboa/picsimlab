2a0f4a1e199459ef1dda7e42096b2b6c3f190bb8
lcgamboa <lcgamboa@yahoo.com>
2020-04-27 17:43:27 -0300

    chg: Makefile cross win32 updated !minor

---
+++ picsimlab_build_w32.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#show errors in color red 
+set -o pipefail
+cl()("$@" 2>&1>&3|sed $'s,.*,\e[31m&\e[m,'>&2)3>&1
+
+echo -e "\033[1;32m ---------------------- update and install packages ---------------------- \033[0m"
+cl sudo apt-get update
+cl sudo apt-get -y upgrade
+cl sudo apt-get -y install git make mingw-w64-common mingw-w64-i686-dev  gcc-mingw-w64-i686 \
+g++-mingw-w64-i686 wget unzip
+cl wget https://github.com/lcgamboa/picsimlab/releases/download/v0.8/picsimlab_0_8.0_w32-dev.zip
+cl unzip picsimlab_0_8.0_w32-dev.zip
+make FILE=Makefile.cross_32 -j4
