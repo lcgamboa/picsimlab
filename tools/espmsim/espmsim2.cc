6669484ca6f3dbd4e97c3f772e0e2b56e2c96e0a
lcgamboa <lcgamboa@yahoo.com>
2020-05-12 14:53:47 -0300

    tools remove chtml when use external browser

--- tools/espmsim/espmsim2.cc
+++ tools/espmsim/espmsim2.cc
@@ -5,13 +5,14 @@ CPWindow2 Window2;
 
 
 //Implementation
-
+#ifndef EXT_BROWSER
 void
 CPWindow2::_EvOnShow(CControl * control)
 {
    html1.SetWidth(Window2.GetWidth()-30);
    html1.SetHeight(Window2.GetHeight()-50); 
-};
+}
+#endif
 
 
 
