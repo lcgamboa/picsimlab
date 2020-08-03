d30991afc70e1759ffe5928ddac6b1f939ebcaf6
lcgamboa <lcgamboa@yahoo.com>
2020-08-03 00:52:09 -0300

    new: support to ucsim 8051

---
+++ src/boards/board_s51.cc
@@ -0,0 +1,375 @@
+/* ########################################################################
+
+   PICsimLab - PIC laboratory simulator
+
+   ########################################################################
+
+   Copyright (c) : 2015-2020  Luis Claudio Gambôa Lopes
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   For e-mail suggestions :  lcgamboa@yahoo.com
+   ######################################################################## */
+
+//include files
+#include"../picsimlab1.h"
+#include"../picsimlab4.h" //Oscilloscope
+#include"../picsimlab5.h" //Spare Parts
+#include"board_s51.h"
+
+/* ids of inputs of input map*/
+enum
+{
+ I_ICSP, //ICSP connector
+ I_PWR, //Power button
+ I_RST, //Reset button
+};
+
+/* ids of outputs of output map*/
+enum
+{
+ O_LPWR, //Power LED
+ O_LED, //LED on PC13 output
+};
+//return the input ids numbers of names used in input map
+
+unsigned short
+cboard_s51::get_in_id(char * name)
+{
+ if (strcmp (name, "I_ICSP") == 0)return I_ICSP;
+ if (strcmp (name, "I_PWR") == 0)return I_PWR;
+ if (strcmp (name, "I_RST") == 0)return I_RST;
+
+ printf ("Error input '%s' don't have a valid id! \n", name);
+ return -1;
+}
+
+//return the output ids numbers of names used in output map
+
+unsigned short
+cboard_s51::get_out_id(char * name)
+{
+
+ if (strcmp (name, "LED") == 0)return O_LED;
+ if (strcmp (name, "LPWR") == 0)return O_LPWR;
+
+
+ printf ("Error output '%s' don't have a valid id! \n", name);
+ return 1;
+}
+
+//Constructor called once on board creation 
+
+cboard_s51::cboard_s51(void)
+{
+ Proc = "C51"; //default microcontroller if none defined in preferences
+ ReadMaps (); //Read input and output board maps
+}
+
+//Destructor called once on board destruction 
+
+cboard_s51::~cboard_s51(void)
+{
+
+}
+
+//Reset board status
+
+void
+cboard_s51::Reset(void)
+{
+ MReset(1);
+
+ //verify serial port state and refresh status bar  
+#ifndef _WIN_
+ if (serialfd > 0)
+#else
+ if (serial[0].serialfd != INVALID_HANDLE_VALUE)
+#endif
+  Window1.statusbar1.SetField (2, lxT ("Serial: ") +
+                               String::FromAscii (SERIALDEVICE) + lxT (":") + itoa (serialbaud) + lxT ("(") +
+                               String ().Format ("%4.1f", fabs ((100.0 * serialexbaud - 100.0 *
+                                                                 serialbaud) / serialexbaud)) + lxT ("%)"));
+ else
+  Window1.statusbar1.SetField (2, lxT ("Serial: ") +
+                               String::FromAscii (SERIALDEVICE) + lxT (" (ERROR)"));
+
+ if (use_spare)Window5.Reset ();
+}
+
+//Called ever 1s to refresh status
+
+void
+cboard_s51::RefreshStatus(void)
+{
+ //verify serial port state and refresh status bar   
+#ifndef _WIN_
+ if (serialfd > 0)
+#else
+ if (pic.serial[0].serialfd != INVALID_HANDLE_VALUE)
+#endif
+  Window1.statusbar1.SetField (2, lxT ("Serial: ") +
+                               String::FromAscii (SERIALDEVICE) + lxT (":") + itoa (serialbaud) + lxT ("(") +
+                               String ().Format ("%4.1f", fabs ((100.0 * serialexbaud - 100.0 *
+                                                                 serialbaud) / serialexbaud)) + lxT ("%)"));
+ else
+  Window1.statusbar1.SetField (2, lxT ("Serial: ") +
+                               String::FromAscii (SERIALDEVICE) + lxT (" (ERROR)"));
+
+}
+
+//Called to save board preferences in configuration file
+
+void
+cboard_s51::WritePreferences(void)
+{
+ //write selected microcontroller of board_x to preferences
+ Window1.saveprefs (lxT ("s51_proc"), Proc);
+ //write microcontroller clock to preferences
+ Window1.saveprefs (lxT ("s51_clock"), String ().Format ("%2.1f", Window1.GetClock())); 
+}
+
+//Called whe configuration file load  preferences 
+
+void
+cboard_s51::ReadPreferences(char *name, char *value)
+{
+
+ //read microcontroller of preferences
+ if (!strcmp (name, "s51_proc"))
+  {
+   Proc = value;
+  }
+ //read microcontroller clock
+ if (!strcmp (name, "s51_clock"))
+ {
+  Window1.SetClock (atof(value));
+ } 
+}
+
+
+//Event on the board
+
+void
+cboard_s51::EvKeyPress(uint key, uint mask)
+{
+ 
+}
+
+//Event on the board
+
+void
+cboard_s51::EvKeyRelease(uint key, uint mask)
+{
+
+}
+
+//Event on the board
+
+void
+cboard_s51::EvMouseButtonPress(uint button, uint x, uint y, uint state)
+{
+
+ int i;
+
+ //search for the input area which owner the event    
+ for (i = 0; i < inputc; i++)
+  {
+   if (((input[i].x1 <= x)&&(input[i].x2 >= x))&&((input[i].y1 <= y)&&
+                                                  (input[i].y2 >= y)))
+    {
+
+     switch (input[i].id)
+      {
+       //if event is over I_ISCP area then load hex file  
+      case I_ICSP:
+       Window1.menu1_File_LoadHex_EvMenuActive (NULL);
+       break;
+       //if event is over I_PWR area then toggle board on/off
+      case I_PWR:
+       if (Window1.Get_mcupwr ()) //if on turn off
+        {
+         Window1.Set_mcurun (0);
+         Window1.Set_mcupwr (0);
+         Reset ();
+         Window1.statusbar1.SetField (0, lxT ("Stoped"));
+        }
+       else //if off turn on
+        {
+         Window1.Set_mcupwr (1);
+         Window1.Set_mcurun (1);
+         Reset ();
+         Window1.statusbar1.SetField (0, lxT ("Running..."));
+        }
+       break;
+       //if event is over I_RST area then turn off and reset
+      case I_RST:
+       /*
+       if (Window1.Get_mcupwr () && reset (-1))//if powered
+        {
+         Window1.Set_mcupwr (0);
+         Window1.Set_mcurst (1);
+        }
+        */ 
+       MReset(-1);
+       p_MCLR = 0;
+       break;
+      }
+    }
+  }
+
+}
+
+//Event on the board
+
+void
+cboard_s51::EvMouseButtonRelease(uint button, uint x, uint y, uint state)
+{
+ int i;
+
+ //search for the input area which owner the event  
+ for (i = 0; i < inputc; i++)
+  {
+   if (((input[i].x1 <= x)&&(input[i].x2 >= x))&&((input[i].y1 <= y)&&
+                                                  (input[i].y2 >= y)))
+    {
+     switch (input[i].id)
+      {
+       //if event is over I_RST area then turn on  
+      case I_RST:
+       if (Window1.Get_mcurst ())//if powered
+        {
+         Window1.Set_mcupwr (1);
+         Window1.Set_mcurst (0);
+/*
+         if (reset (-1))
+          {
+           Reset ();
+          }
+ */ 
+        }
+       p_MCLR = 1;
+       break;
+      }
+    }
+  }
+
+}
+
+
+//Called ever 100ms to draw board
+//This is the critical code for simulator running speed
+
+void
+cboard_s51::Draw(CDraw *draw, double scale)
+{
+ int i;
+
+ draw->Canvas.Init (scale, scale); //initialize draw context
+
+ //board_x draw 
+ for (i = 0; i < outputc; i++) //run over all outputs
+  {
+   if (!output[i].r)//if output shape is a rectangle
+    {
+  
+     draw->Canvas.SetFgColor (0, 0, 0); //black
+
+     switch (output[i].id)//search for color of output
+      {
+      case O_LED: //White using pc13 mean value 
+       draw->Canvas.SetColor (pins[1].oavalue, 0 , 0);
+       break;
+      case O_LPWR: //Blue using mcupwr value
+       draw->Canvas.SetColor (225 * Window1.Get_mcupwr () + 30, 0 ,0 );
+       break;
+      }
+
+     draw->Canvas.Rectangle (1, output[i].x1, output[i].y1, output[i].x2 - output[i].x1, output[i].y2 - output[i].y1);
+    }
+
+  }
+
+ //end draw
+ draw->Canvas.End ();
+ draw->Update ();
+
+}
+
+void
+cboard_s51::Run_CPU(void)
+{
+ int i;
+ int j;
+ unsigned char pi;
+ unsigned int alm[64];
+
+ int JUMPSTEPS = Window1.GetJUMPSTEPS (); //number of steps skipped
+ long int NSTEPJ = Window1.GetNSTEPJ (); //number of steps in 100ms
+
+
+ //reset pins mean value
+ memset (alm, 0, 64* sizeof (unsigned int));
+
+
+ //Spare parts window pre process
+ if (use_spare)Window5.PreProcess ();
+
+ j = JUMPSTEPS; //step counter
+ if (Window1.Get_mcupwr ()) //if powered
+  for (i = 0; i < Window1.GetNSTEP (); i++) //repeat for number of steps in 100ms
+   {
+
+    /*
+    if (j >= JUMPSTEPS)//if number of step is bigger than steps to skip 
+     {
+     }
+    */
+    //verify if a breakpoint is reached if not run one instruction 
+    MStep ();
+    //Oscilloscope window process
+    if (use_oscope)Window4.SetSample ();
+    //Spare parts window process
+    if (use_spare)Window5.Process ();
+
+    //increment mean value counter if pin is high 
+    if (j < MGetPinCount ())
+     alm[j] += pins[j].value;
+
+    if (j >= JUMPSTEPS)//if number of step is bigger than steps to skip 
+     {
+      j = -1; //reset counter
+     }
+    
+    j++; //counter increment
+   }
+
+ //calculate mean value
+ for (pi = 0; pi < MGetPinCount (); pi++)
+  {
+   pins[pi].oavalue = (int) (((225.0 * alm[pi]) / NSTEPJ) + 30);
+  }
+ 
+ //Spare parts window pre post process
+ if (use_spare)Window5.PostProcess ();
+
+}
+
+#ifdef _EXPERIMENTAL_
+
+//Register the board in PICSimLab
+board_init("s51", cboard_s51); 
+
+#endif
---
+++ src/boards/board_ucsim.cc
@@ -0,0 +1,333 @@
+/* ########################################################################
+
+   PICsimLab - PIC laboratory simulator
+
+   ########################################################################
+
+   Copyright (c) : 2010-2020  Luis Claudio Gambôa Lopes
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   For e-mail suggestions :  lcgamboa@yahoo.com
+   ######################################################################## */
+
+
+#include "../../../ucsim-0.6/s51_lib.h"
+
+#include "board_ucsim.h"
+#include"../picsimlab1.h"
+
+
+const unsigned char UCSIM_PORTS[5] = {0,1,2,3,0xFF};
+
+board_ucsim::board_ucsim(void) { }
+
+void
+board_ucsim::MSetSerial(const char * port) {
+ /*
+ set_serial (&pic,0, port, 0, 0, 0);
+ set_serial (&pic,1, "", 0, 0, 0);
+  */ }
+
+int
+board_ucsim::MInit(const char * processor, const char * fname, float freq)
+{
+
+
+ pins_reset ();
+
+ s51_init ("C51", "8M", fname, "/dev/tnt2");
+
+ return 0; //ret;
+}
+
+void
+board_ucsim::MEnd(void) {
+ s51_end(); //TODO
+}
+
+void
+board_ucsim::MEraseFlash(void) {
+ //erase_flash ();
+}
+
+void
+board_ucsim::MSetFreq(float freq_)
+{
+ freq = freq_;
+}
+
+float
+board_ucsim::MGetFreq(void)
+{
+ return freq;
+}
+
+float
+board_ucsim::MGetInstClock(void)
+{
+ return freq;
+}
+
+int
+board_ucsim::CpuInitialized(void)
+{
+ return 1;
+}
+
+void
+board_ucsim::DebugLoop(void) { }
+
+String
+board_ucsim::MGetPinName(int pin)
+{
+ String pinname = "error";
+
+ switch (pin)
+  {
+  case 1:
+   pinname = "P1.0";
+   break;
+  case 2:
+   pinname = "P1.1";
+   break;
+  case 3:
+   pinname = "P1.2";
+   break;
+  case 4:
+   pinname = "P1.3";
+   break;
+  case 5:
+   pinname = "P1.4";
+   break;
+  case 6:
+   pinname = "P1.5";
+   break;
+  case 7:
+   pinname = "P1.6";
+   break;
+  case 8:
+   pinname = "P1.7";
+   break;
+  case 9:
+   pinname = "RST";
+   break;
+  case 10:
+   pinname = "P3.0";
+   break;
+  case 11:
+   pinname = "P3.1";
+   break;
+  case 12:
+   pinname = "P3.2";
+   break;
+  case 13:
+   pinname = "P3.3";
+   break;
+  case 14:
+   pinname = "P3.4";
+   break;
+  case 15:
+   pinname = "P3.5";
+   break;
+  case 16:
+   pinname = "P3.6";
+   break;
+  case 17:
+   pinname = "P3.7";
+   break;
+  case 18:
+   pinname = "XTAL2";
+   break;
+  case 19:
+   pinname = "XTAL1";
+   break;
+  case 20:
+   pinname = "GND";
+   break;
+  case 21:
+   pinname = "P2.0";
+   break;
+  case 22:
+   pinname = "P2.1";
+   break;
+  case 23:
+   pinname = "P2.2";
+   break;
+  case 24:
+   pinname = "P2.3";
+   break;
+  case 25:
+   pinname = "P2.4";
+   break;
+  case 26:
+   pinname = "P2.5";
+   break;
+  case 27:
+   pinname = "P2.6";
+   break;
+  case 28:
+   pinname = "P2.7";
+   break;
+  case 29:
+   pinname = "/PSEN";
+   break;
+  case 30:
+   pinname = "ALE";
+   break;
+  case 31:
+   pinname = "/EA";
+   break;
+  case 32:
+   pinname = "P0.7";
+   break;
+  case 33:
+   pinname = "P0.6";
+   break;
+  case 34:
+   pinname = "P0.5";
+   break;
+  case 35:
+   pinname = "P0.4";
+   break;
+  case 36:
+   pinname = "P0.3";
+   break;
+  case 37:
+   pinname = "P0.2";
+   break;
+  case 38:
+   pinname = "P0.1";
+   break;
+  case 39:
+   pinname = "P0.0";
+   break;
+  case 40:
+   pinname = "VCC";
+   break;
+  }
+ return pinname;
+}
+
+void
+board_ucsim::MDumpMemory(const char * fname) 
+{ 
+   s51_dump (fname);
+}
+
+int
+board_ucsim::DebugInit(int dtyppe) //argument not used in picm only mplabx
+{
+ return 0; //!mplabxd_init (this, Window1.Get_debug_port ()) - 1;
+}
+
+int
+board_ucsim::MGetPinCount(void)
+{
+ return 40;
+}
+
+void
+board_ucsim::pins_reset(void)
+{
+ for (int p = 0; p < MGetPinCount (); p++)
+  {
+   String pname = MGetPinName (p + 1);
+   if (pname[0] == 'P')
+    {
+     pins[p].port = (unsigned char *)&UCSIM_PORTS[pname[1] - '0'];
+     pins[p].pord = pname[3] - '0';
+    }
+   else
+    {
+     pins[p].port = (unsigned char *)&UCSIM_PORTS[4];
+     pins[p].pord = -1;
+    }
+   pins[p].avalue = 0;
+   pins[p].lvalue = 0;
+   pins[p].value = 0;
+   pins[p].ptype = PT_CMOS;
+   pins[p].dir = PD_IN;
+   pins[p].ovalue = 0;
+   pins[p].oavalue = 0;
+  }
+
+ //TODO
+ //VCC pins
+ //pins[6].value = 1;
+ //pins[19].value = 1;
+}
+
+void
+board_ucsim::MSetPin(int pin, unsigned char value) 
+{ 
+ s51_set_pin (*pins[pin-1].port, pins[pin-1].pord, value);
+}
+
+void
+board_ucsim::MSetPinDOV(int pin, unsigned char ovalue) {
+ //set_pin_DOV (pin, ovalue);
+}
+
+void
+board_ucsim::MSetAPin(int pin, float value) {
+ //set_apin (pin, value);
+}
+
+unsigned char
+board_ucsim::MGetPin(int pin)
+{
+ return s51_get_pin (*pins[pin-1].port, pins[pin-1].pord);
+}
+
+void
+board_ucsim::MReset(int flags) 
+{ 
+ s51_reset ();
+}
+
+const picpin *
+board_ucsim::MGetPinsValues(void)
+{
+ return pins;
+}
+
+void
+board_ucsim::MStep(void)
+{
+ unsigned char p[4];
+ 
+ s51_step ();
+ 
+ p[0]=s51_get_port (0);
+ p[1]=s51_get_port (1);
+ p[2]=s51_get_port (2);
+ p[3]=s51_get_port (3);
+ 
+ for(int i=0; i< MGetPinCount (); i++)
+  {
+    if(*pins[i].port < 4)
+     {
+      pins[i].value = (p[*pins[i].port] &  (1<< pins[i].pord))>0;
+     }
+  }
+ 
+}
+
+void
+board_ucsim::MStepResume(void) {
+ //if (pic.s2 == 1)step ();
+}
+
