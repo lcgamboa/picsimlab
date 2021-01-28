39a7af468f480b23302075f5f2600b217e6448a4
lcgamboa <lcgamboa@yahoo.com>
2021-01-28 00:22:33 -0300

    chg: script updated !minor

--- picsimlab_build_appimage.sh
+++ picsimlab_build_appimage.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/bash -x
 . VERSION
 
 export VERSION=${VERSION}
@@ -10,16 +10,32 @@ make DESTDIR=`pwd`/AppDir install_app
 rm -rf AppDir/usr/share/picsimlab/docs/
 cp /usr/bin/cutecom AppDir/usr/bin
 cp /usr/bin/gtkwave AppDir/usr/bin
+install -d  "AppDir/usr/share/glib-2.0/schemas"
+cp /usr/share/glib-2.0/schemas/com.geda.gtkwave.gschema.xml AppDir/usr/share/glib-2.0/schemas
+glib-compile-schemas AppDir/usr/share/glib-2.0/schemas/ || echo "No AppDir/usr/share/glib-2.0/schemas/"
+if [[ -n "$1" ]]; then
+  cp /usr/local/bin/qemu-stm32 AppDir/usr/bin
+  teste/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/qemu-stm32
+fi
 teste/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/cutecom
 teste/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/gtkwave
-teste/linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+rm -rf /tmp/AppRun
+echo "#!/usr/bin/bash" >> /tmp/AppRun
+echo "" >> /tmp/AppRun
+echo "export APPDIR=\"\$(dirname \"\$(readlink -f \"\$0\")\")\"" >> /tmp/AppRun
+echo "export PATH=\"\$APPDIR/usr/bin/:\$PATH\"" >> /tmp/AppRun
+echo "export LD_LIBRARY_PATH=\"\$APPDIR/usr/lib:\$PATH\"" >> /tmp/AppRun
+echo "export XDG_DATA_DIRS=\"\$APPDIR/usr/share/:/usr/share/:\$XDG_DATA_DIRS\"" >> /tmp/AppRun
+echo "" >> /tmp/AppRun
+echo "\$APPDIR/usr/bin/picsimlab \$@" >> /tmp/AppRun
+chmod a+x /tmp/AppRun
+teste/linuxdeploy-x86_64.AppImage --custom-apprun=/tmp/AppRun --appdir AppDir --output appimage
 if [[ -n "$1" ]]; then
   mv PICSimLab-${VERSION}-x86_64.AppImage release_${VERSION}/PICSimLab-${VERSION}_experimetal-x86_64.AppImage 
 else
   mv PICSimLab-${VERSION}-x86_64.AppImage release_${VERSION}
 fi	
 rm -rf AppDir
-
 
 cd src
 rm -rf AppDir
