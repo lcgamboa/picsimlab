534a9eba3571ae5fe3653fd521ef4aad813bdd3e
lcgamboa <lcgamboa@yahoo.com>
2021-01-28 08:27:42 -0300

    chg: scripts updated !minor

--- picsimlab_build_appimage.sh
+++ picsimlab_build_appimage.sh
@@ -13,12 +13,14 @@ cp /usr/bin/gtkwave AppDir/usr/bin
 install -d  "AppDir/usr/share/glib-2.0/schemas"
 cp /usr/share/glib-2.0/schemas/com.geda.gtkwave.gschema.xml AppDir/usr/share/glib-2.0/schemas
 glib-compile-schemas AppDir/usr/share/glib-2.0/schemas/ || echo "No AppDir/usr/share/glib-2.0/schemas/"
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+mv linuxdeploy-x86_64.AppImage /tmp/
 if [[ -n "$1" ]]; then
   cp /usr/bin/qemu-stm32 AppDir/usr/bin
-  teste/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/qemu-stm32
+  /tmp/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/qemu-stm32
 fi
-teste/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/cutecom
-teste/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/gtkwave
+/tmp/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/cutecom
+/tmp/linuxdeploy-x86_64.AppImage --appdir AppDir --executable=AppDir/usr/bin/gtkwave
 rm -rf /tmp/AppRun
 echo "#!/usr/bin/bash" >> /tmp/AppRun
 echo "" >> /tmp/AppRun
@@ -29,7 +31,7 @@ echo "export XDG_DATA_DIRS=\"\$APPDIR/usr/share/:/usr/share/:\$XDG_DATA_DIRS\"" 
 echo "" >> /tmp/AppRun
 echo "\$APPDIR/usr/bin/picsimlab \$@" >> /tmp/AppRun
 chmod a+x /tmp/AppRun
-teste/linuxdeploy-x86_64.AppImage --custom-apprun=/tmp/AppRun --appdir AppDir --output appimage
+/tmp/linuxdeploy-x86_64.AppImage --custom-apprun=/tmp/AppRun --appdir AppDir --output appimage
 if [[ -n "$1" ]]; then
   mv PICSimLab-${VERSION}-x86_64.AppImage release_${VERSION}/PICSimLab-${VERSION}_experimetal-x86_64.AppImage 
 else
@@ -47,7 +49,7 @@ cd AppDir/usr/share/picsimlab
 find . -type f -name '*.png' -exec rm {} +
 find . -type f -name '*.xcf' -exec rm {} +
 cd -
-../teste/linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+/tmp/linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 if [[ -n "$1" ]]; then
   mv PICSimLab_NOGUI-${VERSION}-x86_64.AppImage ../release_${VERSION}/PICSimLab_NOGUI-${VERSION}_experimental-x86_64.AppImage 
 else
