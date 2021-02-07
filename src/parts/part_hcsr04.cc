ab1f4d1d2b5c0bb130ccee9dced6304b7e77d01c
lcgamboa <lcgamboa@yahoo.com>
2021-02-07 17:35:17 -0300

    new: Part Ultrasonic HC-SR04 added

---
+++ src/parts/part_hcsr04.cc
@@ -0,0 +1,326 @@
+/* ########################################################################
+
+   PICsimLab - PIC laboratory simulator
+
+   ########################################################################
+
+   Copyright (c) : 2010-2020  Luis Claudio Gambôa Lopes
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   For e-mail suggestions :  lcgamboa@yahoo.com
+   ######################################################################## */
+
+#include"../picsimlab1.h"
+#include"../picsimlab4.h"
+#include"../picsimlab5.h"
+#include"part_hcsr04.h"
+
+/* outputs */
+enum
+{
+ O_P1, O_P2, O_PO1
+};
+
+/* inputs */
+enum
+{
+ I_PO1
+};
+
+cpart_hcsr04::cpart_hcsr04(unsigned x, unsigned y)
+{
+ X = x;
+ Y = y;
+ ReadMaps ();
+
+ lxImage image;
+ image.LoadFile (Window1.GetSharePath () + lxT ("parts/") + GetPictureFileName ());
+
+ Bitmap = lxGetBitmapRotated (&image, &Window5, orientation);
+ image.Destroy ();
+
+ canvas.Create (Window5.GetWWidget (), Bitmap);
+
+
+ input_pins[0] = 0;
+
+ output_pins[0] = 0;
+
+ value = 0;
+ active = 0;
+ old_value = 0;
+ delay = 0;
+ count = 0;
+
+ RegisterRemoteControl ();
+}
+
+void
+cpart_hcsr04::RegisterRemoteControl(void)
+{
+ for (int i = 0; i < inputc; i++)
+  {
+   switch (input[i].id)
+    {
+    case I_PO1:
+     input[i].status = &value;
+     break;
+    }
+  }
+}
+
+cpart_hcsr04::~cpart_hcsr04(void)
+{
+ delete Bitmap;
+ canvas.Destroy ();
+}
+
+void
+cpart_hcsr04::Draw(void)
+{
+
+ int i;
+ char val[10];
+
+ canvas.Init (1.0, 1.0, orientation);
+
+ lxFont font (9, lxFONTFAMILY_TELETYPE, lxFONTSTYLE_NORMAL, lxFONTWEIGHT_BOLD);
+ lxFont font_p (6, lxFONTFAMILY_TELETYPE, lxFONTSTYLE_NORMAL, lxFONTWEIGHT_BOLD);
+ canvas.SetFont (font);
+
+ for (i = 0; i < outputc; i++)
+  {
+
+   switch (output[i].id)
+    {
+    case O_P1:
+     canvas.SetColor (49, 61, 99);
+     canvas.Rectangle (1, output[i].x1, output[i].y1, output[i].x2 - output[i].x1, output[i].y2 - output[i].y1);
+     canvas.SetFgColor (255, 255, 255);
+     if (input_pins[output[i].id - O_P1] == 0)
+      canvas.RotatedText ("NC", output[i].x1, output[i].y2, 90);
+     else
+      canvas.RotatedText (Window5.GetPinName (input_pins[output[i].id - O_P1]), output[i].x1, output[i].y2, 90);
+     break;
+    case O_P2:
+     canvas.SetColor (49, 61, 99);
+     canvas.Rectangle (1, output[i].x1, output[i].y1, output[i].x2 - output[i].x1, output[i].y2 - output[i].y1);
+     canvas.SetFgColor (255, 255, 255);
+     if (output_pins[output[i].id - O_P2] == 0)
+      canvas.RotatedText ("NC", output[i].x1, output[i].y2, 90);
+     else
+      canvas.RotatedText (Window5.GetPinName (output_pins[output[i].id - O_P2]), output[i].x1, output[i].y2, 90);
+     break;
+    case O_PO1:
+     canvas.SetColor (50, 50, 50);
+     canvas.Rectangle (1, output[i].x1, output[i].y1, output[i].x2 - output[i].x1, output[i].y2 - output[i].y1);
+     canvas.SetColor (250, 250, 250);
+     snprintf (val, 10, "%4.2f", 4.0 * (200 - value) / 200.0);
+     canvas.Rectangle (1, output[i].x1 + 6, output[i].y1 + value / 1.35, 20, 10);
+     canvas.SetColor (150, 0, 0);
+     canvas.SetFont (font_p);
+     canvas.RotatedText (val, output[i].x1 + 6, output[i].y1 + value / 1.35, 0);
+     break;
+    }
+  }
+
+ canvas.End ();
+
+}
+
+void
+cpart_hcsr04::Process(void)
+{
+ const picpin * pins = Window5.GetPinsValues ();
+
+
+ if ((input_pins[0])&&(output_pins[0]))
+  {
+   //tigger
+   if ((!pins[input_pins[0] - 1].value)&& (old_value))
+    {
+     //200us  8 cycles of 40KHz
+     delay = 200e-6 * Window1.GetBoard ()->MGetInstClockFreq ();
+     count = 0;
+    }
+   old_value = pins[input_pins[0] - 1].value;
+
+   if (delay)
+    {
+     delay--;
+     if (!delay)
+      {
+       count = (200-value) * 116.144018583e-6 * Window1.GetBoard ()->MGetInstClockFreq ();
+       Window5.SetPin (output_pins[0], 1);
+      }
+    }
+
+   if (count)
+    {
+     count--;
+     if (!count)
+      {
+       Window5.SetPin (output_pins[0], 0);
+      }
+    }
+  }
+}
+
+void
+cpart_hcsr04::EvMouseButtonPress(uint button, uint x, uint y, uint state)
+{
+
+ int i;
+
+ for (i = 0; i < inputc; i++)
+  {
+   if (PointInside (x, y, input[i]))
+    {
+     RotateCoords (&x, &y);
+     switch (input[i].id)
+      {
+      case I_PO1:
+       value = (y - input[i].y1)*1.35;
+       if (value > 200)value = 200;
+       active = 1;
+       break;
+      }
+    }
+  }
+
+}
+
+void
+cpart_hcsr04::EvMouseButtonRelease(uint button, uint x, uint y, uint state)
+{
+ int i;
+
+ for (i = 0; i < inputc; i++)
+  {
+   if (PointInside (x, y, input[i]))
+    {
+     switch (input[i].id)
+      {
+      case I_PO1:
+       active = 0;
+       break;
+      }
+    }
+  }
+}
+
+void
+cpart_hcsr04::EvMouseMove(uint button, uint x, uint y, uint state)
+{
+
+ int i;
+
+ for (i = 0; i < inputc; i++)
+  {
+   if (PointInside (x, y, input[i]))
+    {
+     RotateCoords (&x, &y);
+
+     if (active)
+      {
+       value = (y - input[i].y1)*1.35;
+       if (value > 200)value = 200;
+      }
+    }
+  }
+}
+
+unsigned short
+cpart_hcsr04::get_in_id(char * name)
+{
+
+ if (strcmp (name, "PO_1") == 0)return I_PO1;
+
+ printf ("Erro input '%s' don't have a valid id! \n", name);
+ return -1;
+}
+
+unsigned short
+cpart_hcsr04::get_out_id(char * name)
+{
+
+ if (strcmp (name, "PN_1") == 0)return O_P1;
+ if (strcmp (name, "PN_2") == 0)return O_P2;
+
+ if (strcmp (name, "PO_1") == 0)return O_PO1;
+
+ printf ("Erro output '%s' don't have a valid id! \n", name);
+ return 1;
+}
+
+lxString
+cpart_hcsr04::WritePreferences(void)
+{
+ char prefs[256];
+
+ sprintf (prefs, "%hhu,%hhu,%hhu", input_pins[0], output_pins[0], value);
+
+ return prefs;
+}
+
+void
+cpart_hcsr04::ReadPreferences(lxString value_)
+{
+ sscanf (value_.c_str (), "%hhu,%hhu,%hhu", &input_pins[0], &output_pins[0], &value);
+}
+
+void
+cpart_hcsr04::ConfigurePropertiesWindow(CPWindow * WProp)
+{
+ lxString Items = Window5.GetPinsNames ();
+ lxString spin;
+
+ ((CCombo*) WProp->GetChildByName ("combo1"))->SetItems (Items);
+ if (input_pins[0] == 0)
+  ((CCombo*) WProp->GetChildByName ("combo1"))->SetText ("0  NC");
+ else
+  {
+   spin = Window5.GetPinName (input_pins[0]);
+   ((CCombo*) WProp->GetChildByName ("combo1"))->SetText (itoa (input_pins[0]) + "  " + spin);
+  }
+
+ ((CCombo*) WProp->GetChildByName ("combo2"))->SetItems (Items);
+ if (output_pins[0] == 0)
+  ((CCombo*) WProp->GetChildByName ("combo2"))->SetText ("0  NC");
+ else
+  {
+   spin = Window5.GetPinName (output_pins[0]);
+   ((CCombo*) WProp->GetChildByName ("combo2"))->SetText (itoa (output_pins[0]) + "  " + spin);
+  }
+
+
+ ((CButton*) WProp->GetChildByName ("button1"))->EvMouseButtonRelease = EVMOUSEBUTTONRELEASE & CPWindow5::PropButtonRelease;
+ ((CButton*) WProp->GetChildByName ("button1"))->SetTag (1);
+
+ ((CButton*) WProp->GetChildByName ("button2"))->EvMouseButtonRelease = EVMOUSEBUTTONRELEASE & CPWindow5::PropButtonRelease;
+}
+
+void
+cpart_hcsr04::ReadPropertiesWindow(CPWindow * WProp)
+{
+ input_pins[0] = atoi (((CCombo*) WProp->GetChildByName ("combo1"))->GetText ());
+ output_pins[0] = atoi (((CCombo*) WProp->GetChildByName ("combo2"))->GetText ());
+
+}
+
+
+part_init("Ultrasonic HC-SR04", cpart_hcsr04);
+
---
+++ src/parts/part_hcsr04.h
@@ -0,0 +1,70 @@
+	/* ########################################################################
+
+	   PICsimLab - PIC laboratory simulator
+
+	   ########################################################################
+
+	   Copyright (c) : 2010-2015  Luis Claudio Gambôa Lopes
+
+	   This program is free software; you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation; either version 2, or (at your option)
+	   any later version.
+
+	   This program is distributed in the hope that it will be useful,
+	   but WITHOUT ANY WARRANTY; without even the implied warranty of
+	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	   GNU General Public License for more details.
+
+	   You should have received a copy of the GNU General Public License
+	   along with this program; if not, write to the Free Software
+	   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+	   For e-mail suggestions :  lcgamboa@yahoo.com
+	   ######################################################################## */
+
+#ifndef PART_HCSR0404_H
+#define	PART_HCSR0404_H
+
+#include<lxrad.h>
+#include"part.h"
+
+class cpart_hcsr04:public part
+{
+    public:
+      lxString GetName(void){return lxT("Ultrasonic HC-SR04");};
+      lxString GetHelpURL(void){return lxT("Ultrasonic_HC-SR04.html");};
+      lxString GetAboutInfo(void){return lxT("L.C. Gamboa \n <lcgamboa@yahoo.com>");};            
+      cpart_hcsr04(unsigned x, unsigned y);
+      ~cpart_hcsr04(void); 
+      void Draw(void);
+      void Process(void);
+      lxString GetPictureFileName(void){return lxT("hcsr04/hcsr04.png");};
+      lxString GetInputMapFile(void){return lxT("hcsr04/hcsr04_i.map");};
+      lxString GetOutputMapFile(void){return lxT("hcsr04/hcsr04_o.map");};
+      lxString GetPropertiesWindowFile(void){return lxT("hcsr04/hcsr04.lxrad");};
+      void EvMouseButtonPress(uint button, uint x, uint y,uint state);
+      void EvMouseButtonRelease(uint button, uint x, uint y,uint state);
+      void EvMouseMove(uint button, uint x, uint y,uint state);
+      void ConfigurePropertiesWindow(CPWindow *  WProp);
+      void ReadPropertiesWindow(CPWindow * WProp);
+      lxString WritePreferences(void);
+      void ReadPreferences(lxString value);
+      unsigned short get_in_id(char * name);
+      unsigned short get_out_id(char * name);
+    private:
+      void RegisterRemoteControl(void);          
+      unsigned char input_pins[1];
+      unsigned char output_pins[1];
+      unsigned char value;
+      unsigned char active;
+      unsigned int  count;
+      unsigned int  delay;
+      unsigned char old_value;
+};
+
+
+
+
+#endif	/* POT */
+
