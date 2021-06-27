945801dcf9148b534c691d76c4bed52e3374e4d0
lcgamboa <lcgamboa@yahoo.com>
2021-06-27 15:49:00 -0300

    new: New part FM50 temperature sensor

---
+++ src/parts/part_fm50.h
@@ -0,0 +1,65 @@
+/* ########################################################################
+
+   PICsimLab - PIC laboratory simulator
+
+   ########################################################################
+
+   Copyright (c) : 2010-2020  Luis Claudio Gambôa Lopes
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   For e-mail suggestions :  lcgamboa@yahoo.com
+   ######################################################################## */
+
+#ifndef PART_FM50_H
+#define	PART_FM50_H
+
+#include<lxrad.h>
+#include"part.h"
+
+#define	PART_FM50_Name "FM50 (Temperature)"
+
+class cpart_fm50:public part
+{
+    public:
+      lxString GetName(void){return lxT(PART_FM50_Name);};
+      lxString GetAboutInfo(void){return lxT("L.C. Gamboa \n <lcgamboa@yahoo.com>");};            
+      cpart_fm50(unsigned x, unsigned y);
+      ~cpart_fm50(void); 
+      void Draw(void);
+      void PostProcess(void);
+      void EvMouseButtonPress(uint button, uint x, uint y,uint state);
+      void EvMouseButtonRelease(uint button, uint x, uint y,uint state);
+      void EvMouseMove(uint button, uint x, uint y,uint state);
+      void ConfigurePropertiesWindow(CPWindow *  WProp);
+      void ReadPropertiesWindow(CPWindow * WProp);
+      lxString WritePreferences(void);
+      void ReadPreferences(lxString value);
+      unsigned short get_in_id(char * name);
+      unsigned short get_out_id(char * name);
+    private:
+      void RegisterRemoteControl(void);          
+      unsigned char output_pins[1];
+      unsigned char value;
+      unsigned char active;
+      lxFont font;
+      lxFont font_p;
+};
+
+
+
+
+#endif	/* POT */
+
