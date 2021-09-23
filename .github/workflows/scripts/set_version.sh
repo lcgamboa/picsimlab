2a7df702c30f2511bff9efa2582c17534a88e1e1
lcgamboa <lcgamboa@yahoo.com>
2021-09-23 19:52:17 -0300

    chg: CI/CD updated !minor

--- .github/workflows/scripts/set_version.sh
+++ .github/workflows/scripts/set_version.sh
@@ -4,5 +4,5 @@
 set -e
 # Get the last tag which is not the top-of-head continuous tag
 #export GIT_VERSION=$(git describe --tags --exclude "continuous")
-export VERSION=$(VERSION)
-export DATE=$(DATE)
+export VERSION="${VERSION}"
+export DATE="${DATE}"
