8f6cfaa767ac4835627c841f10bc478a89ed0b89
lcgamboa <lcgamboa@yahoo.com>
2024-07-21 15:35:12 -0300

    chg: Build script updated !minor

--- bscripts/build_all_static.sh
+++ bscripts/build_all_static.sh
@@ -9,7 +9,7 @@ cl sudo apt-get update
 cl sudo apt-get -y upgrade
 cl sudo apt-get -y install git doxygen gcc g++ make libwxgtk3.*-dev \
 libelf-dev freeglut3-dev cutecom gcc-avr avr-libc libopenal-dev libncurses-dev gtkwave \
-gedit cmake ccache bison pulseview
+gedit cmake ccache bison flex pulseview
 cl sudo apt-get -y install linux-headers-`uname -r` dkms 
 cl mkdir build_all
 cd build_all
