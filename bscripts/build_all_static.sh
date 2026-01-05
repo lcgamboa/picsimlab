e27d70eea9ea8e7d696f378cf638fd73496c4d06
lcgamboa <lcgamboa@yahoo.com>
2026-01-04 21:57:39 -0300

    chg: Build scritps updated

--- bscripts/build_all_static.sh
+++ bscripts/build_all_static.sh
@@ -9,7 +9,7 @@ cl sudo apt-get update
 cl sudo apt-get -y upgrade
 cl sudo apt-get -y install git doxygen gcc g++ make libwxgtk3.*-dev \
 libelf-dev freeglut3-dev cutecom gcc-avr avr-libc libopenal-dev libncurses-dev gtkwave \
-gedit cmake ccache bison flex file  pulseview
+gedit cmake ccache bison flex file  pulseview libcurl4-openssl-dev
 cl sudo apt-get -y install linux-headers-`uname -r` dkms 
 cl mkdir build_all
 cd build_all
