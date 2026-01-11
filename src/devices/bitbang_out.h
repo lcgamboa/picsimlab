a544164b1327869ed754f6f1da3644e3e91fc5a8
lcgamboa <lcgamboa@yahoo.com>
2026-01-11 15:00:26 -0300

    chg: License date updated !minor

--- src/devices/bitbang_out.h
+++ src/devices/bitbang_out.h
@@ -4,7 +4,7 @@
 
    ########################################################################
 
-   Copyright (c) : 2023-2025  Luis Claudio Gambôa Lopes <lcgamboa@yahoo.com>
+   Copyright (c) : 2023-2026 Luis Claudio Gambôa Lopes <lcgamboa@yahoo.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- src/parts/virtual_VCD_Dump_Mem.h
+++ src/parts/virtual_VCD_Dump_Mem.h
@@ -4,7 +4,7 @@
 
    ########################################################################
 
-   Copyright (c) : 2024-2025  Luis Claudio Gambôa Lopes <lcgamboa@yahoo.com>
+   Copyright (c) : 2024-2026 Luis Claudio Gambôa Lopes <lcgamboa@yahoo.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
